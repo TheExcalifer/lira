1848acbb36b13cf1f75255739de511353ed85b67
Behnam <135609805+TheExcalifer@users.noreply.github.com>
2023-09-25 00:43:01 +0330

    Project Structure

--- README.md
+++ README.md
@@ -11,4 +11,4 @@
 ### Postman Doc Link
 
 You can work with API in postman:
-[NFT API](https://www.postman.com/theexcalifer/workspace/lira)
+[Lira API](https://www.postman.com/theexcalifer/workspace/lira)
---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+const app = express();
+
+dotenv.config({ path: '../.env' });
+
+app.use(express.json());
+
+import authRoutes from './routes/auth/user-auth.js';
+app.use('/auth', authRoutes);
+
+const initilize = async () => {
+  try {
+    const CONNECTION_STRING: any = process.env.CONNECTION_STRING;
+    await mongoose.connect(CONNECTION_STRING);
+    app.listen(process.env.SERVER_PORT);
+  } catch (error) {
+    console.error('Connection string is incorrect');
+  }
+};
+
+initilize();
