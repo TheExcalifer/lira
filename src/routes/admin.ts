8dac42dd2a78dfd79685249bf7d3284c7ccbc11a
Behnam <135609805+TheExcalifer@users.noreply.github.com>
2023-09-25 23:48:03 +0330

    added Admin Model

---
+++ src/routes/admin.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router();
+
+import { login } from '../controllers/lira.js';
+import {} from '../controllers/lira.js';
+router.post('/admin/login', login);
+
+export default router;
