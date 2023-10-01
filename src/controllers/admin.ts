8286a01cc4a251a58e4c129f56c6669c723f16b5
Behnam <135609805+TheExcalifer@users.noreply.github.com>
2023-10-01 14:58:11 +0330

    change admin password implemented

--- src/controllers/admin.ts
+++ src/controllers/admin.ts
@@ -45,3 +45,43 @@ export const login: RequestHandler = async (req, res) => {
     res.status(500).json();
   }
 };
+export const changePassword: RequestHandler = async (req, res) => {
+  try {
+    const { oldPassword, newPassword, confirmNewPassword } = req.body;
+
+    // ? Validation
+    const validationSchema = Joi.object().keys({
+      newPassword: Joi.string().required().trim().min(8).max(128),
+      confirmNewPassword: Joi.string().required().trim().min(8).max(128),
+    });
+    const { value: validatedBody, error: validationError } = validationSchema.validate({
+      newPassword,
+      confirmNewPassword,
+    });
+
+    // ? Validation Error
+    if (validationError) return res.status(400).json(validationError);
+
+    // ? Check if the new password is the same and confirm the new password
+    if (newPassword !== confirmNewPassword)
+      return res.status(400).json({ error: 'New password and confirm password does not match.' });
+
+    // ? Finding Admin
+    const user = await Admin.findOne({ _id: req.admin._id });
+    if (!user) return res.status(404).json();
+
+    // ? Checking password correctness
+    const matchedPassword = await bcrypt.compare(oldPassword, user.password);
+    if (!matchedPassword) return res.status(400).json({ error: 'Your password is incorrect.' });
+
+    // ? Encrypt Password
+    const encryptedPassword = await bcrypt.hash(validatedBody.newPassword, 12);
+
+    // ? Update Password
+    await Admin.updateOne({ _id: req.admin._id }, { $set: { password: encryptedPassword } });
+
+    res.status(200).json();
+  } catch (error) {
+    res.status(500).json();
+  }
+};
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 
 const router = Router();
-
-import { login } from '../controllers/admin.js';
+import isAdminAuth from '../middleware/is-admin-auth.js';
+import { login, changePassword } from '../controllers/admin.js';
+router.patch('/change-password', isAdminAuth,changePassword);
 router.post('/auth/login', login);
 
 export default router;
